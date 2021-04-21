e2708598a2ea0266c11ba73a2ddf4d3018f377d4
Patrick Radtke <patrick@cirrusidentity.com>
2021-04-21 16:50:32 -0700

    Add ObjectSidConverter authproc

---
+++ lib/Auth/Process/ObjectSidConverter.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace SimpleSAML\Module\cirrusgeneral\Auth\Process;
+
+use SimpleSAML\Auth\ProcessingFilter;
+use SimpleSAML\Configuration;
+use SimpleSAML\Logger;
+
+/**
+ * ActiveDirectory's objectSid can be a in a binary format or as a formatted string.
+ * Sometimes you'll receive one and expect the other.
+ * @package SimpleSAML\Module\cirrusgeneral\Auth\Process
+ */
+class ObjectSidConverter extends ProcessingFilter
+{
+
+    /**
+     * @var string The attribute to convert from
+     */
+   private string $source;
+
+    /**
+     * @var string Destination to store after the conversion.
+     */
+   private string $destination;
+
+    /**
+     * @var bool convert formatted SID or base64 binary value
+     */
+   private bool $toFormattedSid = false;
+
+
+    public function __construct(&$config, $reserved)
+    {
+        parent::__construct($config, $reserved);
+        $config = Configuration::loadFromArray($config);
+        $this->source = $config->getString('source');
+        $this->destination = $config->getString('destination');
+        $this->toFormattedSid = $config->getBoolean('toFormattedSid', false);
+    }
+
+    /**
+     * Process a request.
+     *
+     * When a filter returns from this function, it is assumed to have completed its task.
+     *
+     * @param array &$request The request we are currently processing.
+     */
+    public function process(&$request)
+    {
+        $attribute = $request['Attributes'][$this->source][0] ?? null;
+        if (empty($attribute)) {
+            return;
+        }
+        $value = $this->toFormattedSid ? self::convertToFormattedSid($attribute) : self::convertToBase64($attribute);
+        $request['Attributes'][$this->destination] = [$value];
+    }
+
+    public static function convertToBase64(string $value): string
+    {
+        // Algorithm https://ldapwiki.com/wiki/ObjectSID
+        // https://www.chadsikorra.com/blog/decode-encode-objectsid
+        $sid = explode('-', ltrim($value, 'S-'));
+
+        $revisionLevel = (int) array_shift($sid);
+        $identifierAuthority = (int) array_shift($sid);
+        $subAuthorities = array_map('intval', $sid);
+
+        $binaryString = pack(
+            'C2xxNV*',
+            $revisionLevel,
+            count($subAuthorities),
+            $identifierAuthority,
+            ...$subAuthorities
+        );
+
+        return base64_encode($binaryString);
+    }
+
+    public static function convertToFormattedSid(string $b64EncodedValue): string
+    {
+        // Algorithm https://ldapwiki.com/wiki/ObjectSID
+        // https://www.chadsikorra.com/blog/decode-encode-objectsid
+        $bytes = base64_decode($b64EncodedValue, true);
+        $sid = @unpack('C1rev/C1count/x2/N1id', $bytes);
+        $subAuthorities = [];
+
+        if (!isset($sid['id']) || !isset($sid['rev'])) {
+            throw new \UnexpectedValueException(
+                'The revision level or identifier authority was not found when decoding the SID.'
+            );
+        }
+
+        $revisionLevel = $sid['rev'];
+        $identifierAuthority = $sid['id'];
+        $subs = isset($sid['count']) ? $sid['count'] : 0;
+
+        // The sub-authorities depend on the count, so only get as many as the count, regardless of data beyond it
+        for ($i = 0; $i < $subs; $i++) {
+            $subAuthorities[] = unpack('V1sub', hex2bin(substr(bin2hex($bytes), 16 + ($i * 8), 8)))['sub'];
+        }
+
+        return 'S-' . $revisionLevel . '-' . $identifierAuthority . implode(
+                preg_filter('/^/', '-', $subAuthorities)
+            );
+    }
+}
---
+++ tests/lib/Auth/Process/ObjectSidConverter.php
@@ -0,0 +1,82 @@
+<?php
+
+
+namespace Test\SimpleSAML\Auth\Process;
+
+
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\Module\cirrusgeneral\Auth\Process\ObjectSidConverter;
+
+class ObjectSidConverterTest extends TestCase
+{
+    private string $sid1 = 'S-1-5-21-113936554-849131609-2399885837-221118';
+    private string $sid2 = 'S-1-5-21-113936554-849131609-2399885837-159036';
+
+    private string $b641 = 'AQUAAAAAAAUVAAAAqojKBlm4nDINWguPvl8DAA';
+    private string $b642 = 'AQUAAAAAAAUVAAAAqojKBlm4nDINWguPPG0CAA';
+
+    /**
+     * Test converting b64 values into sids
+     */
+    public function testConvertToSid(): void
+    {
+        $this->assertEquals($this->sid1, ObjectSidConverter::convertToFormattedSid($this->b641), $this->sid1);
+        $this->assertEquals($this->sid1, ObjectSidConverter::convertToFormattedSid($this->b641 . '=='), $this->sid1);
+        $this->assertEquals($this->sid2, ObjectSidConverter::convertToFormattedSid($this->b642), $this->sid2);
+
+        $state = [
+            'Attributes' => [
+                'b64sid' => [$this->b641]
+            ]
+        ];
+        $config = [
+            'source' => 'b64sid',
+            'destination' => 'sid',
+            'toFormattedSid' => true,
+        ];
+        $filter = new ObjectSidConverter($config, null);
+        $filter->process($state);
+        $this->assertEquals($this->sid1, $state['Attributes']['sid'][0]);
+    }
+
+    /**
+     * Test converting b64 values into sids
+     */
+    public function testConvertToB64(): void
+    {
+        // PHP pads the b64 encoding.
+        $this->assertEquals($this->b641 . '==', ObjectSidConverter::convertToBase64($this->sid1), $this->sid1);
+        $this->assertEquals($this->b642 . '==', ObjectSidConverter::convertToBase64($this->sid2), $this->sid2);
+
+        $state = [
+            'Attributes' => [
+                'sid' => [$this->sid1]
+            ]
+        ];
+        $config = [
+            'source' => 'sid',
+            'destination' => 'b64sid',
+        ];
+        $filter = new ObjectSidConverter($config, null);
+        $filter->process($state);
+        $this->assertEquals($this->b641 . '==', $state['Attributes']['b64sid'][0]);
+    }
+
+    public function testNoAttributeIsNoop()
+    {
+        $state = [
+            'Attributes' => [
+                'someAttribute' => ['a']
+            ]
+        ];
+        $config = [
+            'source' => 'sid',
+            'destination' => 'b64sid',
+        ];
+        $expectedState = $state;
+        $filter = new ObjectSidConverter($config, null);
+        $filter->process($state);
+        $this->assertEquals($expectedState, $state);
+    }
+
+}
